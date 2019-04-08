4e8cb27f34fcd1f5b9bbc0fc9d24260e4923245b
Sumanth Kumar Meesala <meesalasumanth1@gmail.com>
2019-04-08 23:44:29 +1000

    login and registration added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,5 @@
 const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
 exports.listUsers = function(req, res) {
   var output = {
@@ -6,4 +7,89 @@ exports.listUsers = function(req, res) {
     message: "Successfully fetched users."
   };
   res.json(output);
+};
+
+exports.register = function(req, res) {
+  User.findOne(
+    {
+      email: req.body.email
+    },
+    function(err, user) {
+      if (err) {
+        throw err;
+      }
+
+      const new_user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      if (user) {
+        res.json({
+          success: false,
+          message: "User already exists."
+        });
+      } else {
+        new_user.save(function(err) {
+          if (err) {
+            throw err;
+          }
+          res.json({
+            success: true,
+            message: "User registration successful."
+          });
+        });
+      }
+    }
+  );
+};
+
+exports.login = function(req, res) {
+  User.findOne(
+    {
+      email: req.body.email
+    },
+    function(err, user) {
+      if (err) {
+        throw err;
+      }
+
+      if (user) {
+        user.comparePassword(req.body.password, function(err, isMatch) {
+          if (err) {
+            throw err;
+          }
+
+          if (isMatch) {
+            const payload = {
+              userid: user._id
+            };
+
+            var token = jwt.sign(payload, "MyContext", {
+              expiresIn: 60 * 60 * 60 * 24
+            });
+
+            res.json({
+              success: true,
+              message: "Access token generation successfull.",
+              token: token,
+              name: user.name,
+              email:user.email
+            });
+          } else {
+            res.json({
+              success: false,
+              message: "Authentication failed. Wrong password."
+            });
+          }
+        });
+      } else {
+        res.json({
+          success: false,
+          message: "Authentication failed. User not found."
+        });
+      }
+    }
+  );
 };
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const user_controller = require("../controllers/user.controller");
+var VerifyToken = require("./VerifyToken");
 
 const router = express.Router();
 
-router.get("/list-users", user_controller.listUsers);
+router.get("/list-users",VerifyToken, user_controller.listUsers);
+router.post("/register", user_controller.register);
+router.post("/login", user_controller.login);
 
 module.exports = router;
