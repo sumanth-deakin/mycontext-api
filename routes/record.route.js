0e681cf18c071968c40f9cd08d9d02ad51d9272f
Sumanth Kumar Meesala <smeesala@deakin.edu.au>
2019-04-27 13:18:15 +1000

    view update delete route added

--- routes/record.route.js
+++ routes/record.route.js
@@ -5,8 +5,11 @@ var VerifyToken = require("./VerifyToken");
 const router = express.Router();
 
 router.post("/addRecord", VerifyToken, record_controller.addRecord);
+router.post("/deleteRecord", VerifyToken, record_controller.deleteRecord);
+router.post("/updateRecord", VerifyToken, record_controller.updateRecord);
 router.post("/listRecords", VerifyToken, record_controller.listRecords);
 router.post("/listOwnerRecords", VerifyToken, record_controller.listOwnerRecords);
+router.post("/viewRecord", VerifyToken, record_controller.viewRecord);
 router.post("/changeOwnership", VerifyToken, record_controller.changeOwnership);
 
 module.exports = router;
